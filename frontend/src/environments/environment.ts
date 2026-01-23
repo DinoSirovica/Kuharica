a9a807e9ff3602c82f77715d16c206bf0368302a
DinoSirovica <dino14sirovica@gmail.com>
2026-01-23 20:44:42 +0100

    Add Google Sign-In setup instructions

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: false,
-  googleClientId: '674358194368-0areh3ol22h7qpci7hci7aeeoqochotp.apps.googleusercontent.com' // Replace with your actual Google Client ID
+  googleClientId: '674358194368-0areh3ol22h7qpci7hci7aeeoqochotp.apps.googleusercontent.com'
 };
 
